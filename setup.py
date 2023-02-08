835b0f4a81c0e1e398ede1567c491c60f0f83d31
n-j-p <nicholas.j.potter@gmail.com>
2023-02-08 17:33:03 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ setup(
 	author='n-j-p',
 	author_email='example@github.com',
 	packages=['sklearn_named_pipeline'],
-	install_requires=['sklearn'
-	],
+	install_requires=['sklearn', 
+			  'sklearn.impute', 
+			  'sklearn.preprocessing',
+			  'sklearn.compose',
+	                 ],
 )
