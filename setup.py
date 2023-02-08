f0425f59af847ad5ae66a301a3a21220c0db3fed
n-j-p <nicholas.j.potter@gmail.com>
2023-02-08 17:28:54 +1100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name='sklearn_named_pipeline',
+	version='0.1',
+	description='Named transformers for sklearn pipeline',
+	author='n-j-p',
+	packages=['sklearn_named_pipeline'],
+	install_requires=[
+	],
+)
